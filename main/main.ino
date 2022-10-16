cdebb29f83af9055bc2f6afa3eb65fb491a21548
alphaseneca <alphatheseneca@protonmail.com>
2022-10-16 13:08:09 +0545

    Comment added and made a new file for storing wifi_credentials

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,10 @@
 /* Started Date : 14 Oct 2022 */
 /* TemperatureControlledKettle code is here . It is running the 
 communication through blynk server and the new BLYNK 2.0 platform. */
+/* Author: Ukesh Aryal */
+/* https://github.com/alphaseneca */
 
-#define BLYNK_TEMPLATE_ID "TMPLxxxxxx"
-#define BLYNK_DEVICE_NAME "Device"
-#define BLYNK_AUTH_TOKEN "YourAuthToken"
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-
-#define ID "0000"     
-#define PASSWORD "0000"
+#include "secret.h"
 
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
@@ -23,104 +17,129 @@ char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = ID;
 char pass[] = PASSWORD;
 
-BlynkTimer timer;  // Initialize timer as a constructor of object BlynkTimer
-#define stat_pin D1
-#define relay_pin D7
-#define probe_pin A0
+BlynkTimer timer;         // Initialize timer as a constructor of object BlynkTimer
+#define stat_pin D1       // Get the status of the kettle whether it is in Auto or Manual
+#define relay_pin D7      // Relay for the heater
+                          /* Future Implementation Power MOSFET to dim the heater and Obtain PID controller*/
+#define probe_pin A0      //Temperature Probe connected to the analog pin
 
-int state = 0;
+int state = 0;            // Initialization of all the variables
 int mode_select = 0;
 int thres_temp;
 int current_temp;
 int heatersw;
 
-void status() /* Function for checking the mode_status . If the digitalpin reads high 
-then the kettle is in Manual Mode and if reads low then it is in Auto mode */
+void status()           /*  Function for checking the mode_status . If the digitalpin reads high 
+                            then the kettle is in Manual Mode and if reads low then it is in Auto mode
+                        */
 {
   state = digitalRead(stat_pin);
   Blynk.virtualWrite(V0, state);
-  if (state == 1){
+  if (state == 1) {
     heatersw = 0;
     digitalWrite(relay_pin, LOW);
-    Blynk.virtualWrite(V5, heatersw);  }
+    Blynk.virtualWrite(V5, heatersw);
+  }
 }
 
-BLYNK_WRITE(V1) {
+BLYNK_WRITE(V1)     /*  Normally Cut Off mode is selected as we have initialized the mode_select value as 0 
+                        But when we want to manitain the temperature then this fumction gets called when there 
+                        is change in data and the data i.e 1 is set to the mode_select 
+                    */
+{
   mode_select = param.asInt();
 }
 
-BLYNK_WRITE(V2) {  // Get the value from the slider_ set temperature
-  thres_temp = param.asInt();
+BLYNK_WRITE(V2)
+{
+  thres_temp = param.asInt();             // Get the value from the slider_ set temperature
   Serial.println(thres_temp);
-Blynk.virtualWrite(V3, thres_temp); // Display the set temperature to the display at V3 
-
+  Blynk.virtualWrite(V3, thres_temp);     // Display the set temperature to the display at V3
 }
 
-void readtemperature() { /* Function reading the current temperature and sending the data to 
-the virtual pin V4 and setting the value to the current_temp*/
+void readtemperature() {  /*  Function reading the current temperature and sending the data to 
+                              the virtual pin V4 and setting the value to the current_temp 
+                          */
+
   current_temp = analogRead(probe_pin);
-  current_temp = map(current_temp, 0, 1024, 0, 100);
-  Blynk.virtualWrite(V4, current_temp);
-  tempcontrol();}
+  current_temp = map(current_temp, 0, 1024, 0, 100);  // Mapping the obtained value from 0-1024 to 0-100 and setting that as new value
+  Blynk.virtualWrite(V4, current_temp);               // Uploading the currenttemperature reading to the server
+  tempcontrol();          // Calling the temperature control function
+}
 
-void tempcontrol(){
-  
-  int tempdiff; 
-  int re_activationtemp = 2 ;
-  
-     switch (mode_select){
-    case 0:
-       if (current_temp >= thres_temp){
-    digitalWrite(relay_pin, LOW);
-    heatersw = 0;
-    Blynk.virtualWrite(V5, heatersw );
-    }
-    break;
+void tempcontrol() {      // ALL THE CONTROL LOGIC LIES IN HERE 
 
-    case 1:
-        if (current_temp >= thres_temp){
-    digitalWrite(relay_pin, LOW);
-    } 
-    else if (thres_temp > current_temp && heatersw == 1) {
-       tempdiff = thres_temp - current_temp;
-       if(tempdiff > re_activationtemp){
-        digitalWrite(relay_pin, HIGH);         
-       }   } 
-    }
-  
+  int tempdiff;
+  int re_activationtemp = 5;
+
+  switch (mode_select) {                    // Switch for mode_select value 
+    case 0:                                 // If mode_select == 0 i.e Cut OFF mode
+      if (current_temp >= thres_temp) {     // If the currrent temperarature is equals to or more than the thres_temp then turn off the heater 
+        digitalWrite(relay_pin, LOW);
+        heatersw = 0;                 
+        Blynk.virtualWrite(V5, heatersw);   // Update the value to the server
+        Blynk.virtualWrite(V6, 0);
+        Serial.println("Threshold Temperature reached and heater has been turned off");
+      }
+      else 
+        Serial.println("Waiting..."); 
+      break;
+
+    case 1:                                 // If mode_select == 1 i.e Maintain mode
+      if (current_temp >= thres_temp) {
+        digitalWrite(relay_pin, LOW);
+        Blynk.virtualWrite(V6, 0);
+        Serial.println("Threshold Temperature reached and heater has been turned off");        
+                
+      }
+       else if (thres_temp > current_temp && heatersw == 1) {
+        tempdiff = thres_temp - current_temp;
+        if (tempdiff > re_activationtemp) {
+          digitalWrite(relay_pin, HIGH);
+          Blynk.virtualWrite(V6, 1);
+          Serial.println("Heater has been reactivated to maintain the temperature");
+        }
+        else
+        Serial.println("Waiting...");       
+      }
+  }
 }
 
 BLYNK_WRITE(V5) {
   heatersw = param.asInt();
-  if (state == 0 && heatersw == 1) {
+                                              /*  Checks if the status is in Manual or Auto, only if the 
+                                                  status is in auto mode the heater switch is turned on 
+                                              */
+  if (state == 0 && heatersw == 1) 
+  {
     digitalWrite(relay_pin, HIGH);
+    Blynk.virtualWrite(V6, 1);
     Serial.println("Heater Turned On");
+  }
+   else if (state == 0 && heatersw == 0) {
+    digitalWrite(relay_pin, LOW);
+    Blynk.virtualWrite(V6, 0);   
+    Serial.println("Heater Turned Off");
+  } else
+    Serial.println("Cannot turn on the heater. Set the heater to AUTO mode ");
 }
-else  if (state == 0 && heatersw == 0) 
-{
-  digitalWrite(relay_pin, LOW);
-  Serial.println("Heater Turned Off");
-  }
- else 
-  Serial.println("Cannot turn on the heater. Set the heater to AUTO mode ") ;
+
+
+
+void setup() {
+  Serial.begin(9600);  //Initialize serial communication
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  pinMode(relay_pin, OUTPUT);
+
+  timer.setInterval(1000L, status);
+  timer.setInterval(1000L, readtemperature);  // Calling the readtemperature function every second.
 }
 
-
-
-  void setup() {
-    Serial.begin(9600);  //Initialize serial communication
-    Blynk.begin(auth, ssid, pass);
-    // You can also specify server:
-    //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-    //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-    pinMode(relay_pin, OUTPUT);
-    
-    timer.setInterval(1000L, status);
-    timer.setInterval(1000L, readtemperature);   // Calling the status function every second.
-  }
-
-  void loop() {
-    Blynk.run();
-    timer.run();
-  }
+void loop() {
+  Blynk.run();
+  timer.run();
+}
---
+++ main/secret.h
@@ -0,0 +1,9 @@
+#define BLYNK_TEMPLATE_ID "TMPLxxxxxx"
+#define BLYNK_DEVICE_NAME "Device"
+#define BLYNK_AUTH_TOKEN "YourAuthToken"
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+
+#define ID "0000"     
+#define PASSWORD "0000"
